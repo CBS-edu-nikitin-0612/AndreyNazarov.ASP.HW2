c8b7ab8ce0c49d876a44edb353551f84bfd6087b
Andrew Nazarov <xiss@users.noreply.github.com>
2022-07-23 16:40:06 +0300

    Add project files.

---
+++ SimpleApp/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public FileResult Download()
+        {
+            return File(@"Files\001.Introduction.pdf", "application/pdf");
+        }
+    }
+}
---
+++ Task2/Controllers/Calc.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Task2.Controllers
+{
+    public class CalcController : Controller
+    {
+        public IActionResult Add(int left, int right)
+        {
+            return View("Result", left + right);
+        }
+        public IActionResult Mul(int left, int right)
+        {
+            return View("Result", left * right);
+        }
+        public IActionResult Div(int left, int right)
+        {
+            return View("Result", left / right);
+        }
+        public IActionResult Sub(int left, int right)
+        {
+            return View("Result", left - right);
+        }
+    }
+}
